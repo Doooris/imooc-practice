f5f58a92bf002ae602a5f10b1bf28dc7b85dc7d2
Doooris <hxpt2031@gmail.com>
2016-09-21 00:03:26 +0800

    introduction end

--- imooc.js
+++ imooc.js
@@ -11,12 +11,91 @@ $(function(){
 
   }
 );
+$(function(){
+    $(".field1").hover(
+      function(){
+        $(".field1-content").show();
+      },
+      function(){
+        $(".field1-content").hide();
+      }
+    );
+}
+);
+$(function(){
+    $(".field2").hover(
+      function(){
+        $(".field2-content").show();
+      },
+      function(){
+        $(".field2-content").hide();
+      }
+    );
+  }
+);
+$(function(){
+    $(".field3").hover(
+      function(){
+        $(".field3-content").show();
+      },
+      function(){
+        $(".field3-content").hide();
+      }
+    );
+  }
+);
+$(function(){
+    $(".field4").hover(
+      function(){
+        $(".field4-content").show();
+      },
+      function(){
+        $(".field4-content").hide();
+      }
+    );
+  }
+);
+$(function(){
+    $(".field5").hover(
+      function(){
+        $(".field5-content").show();
+      },
+      function(){
+        $(".field5-content").hide();
+      }
+    );
+  }
+);
+$(function(){
+    $(".field6").hover(
+      function(){
+        $(".field6-content").show();
+      },
+      function(){
+        $(".field6-content").hide();
+      }
+    );
+  }
+);
+$(function(){
+    $(".field7").hover(
+      function(){
+        $(".field7-content").show();
+      },
+      function(){
+        $(".field7-content").hide();
+      }
+    );
+  }
+);
 window.onload = function(){
   var container=document.getElementById('container');
   var images=document.getElementById('images');
   var btns=document.getElementById('btns').getElementsByTagName('span');
   var prev=document.getElementById('prev');
   var next=document.getElementById('next');
+  //var field=document.getElementById('menue').getElementsByTagName('li');
+  //var content=document.getElementById('menue-content').getElementsByTagName('div')
   var index=1;
   var timer;
   function animate(offset){
@@ -70,7 +149,7 @@ window.onload = function(){
     showbtn();
     animate(1200);
   };
-  for( i=0;i<btns.length;i++){
+  for( i=0;i<btns.length;i++) {
     btns[i].onclick=function(){
       if(this.className=='on'){
         return;
@@ -81,7 +160,12 @@ window.onload = function(){
       index=myIndex;
       showbtn();
     }
-  }
+  };
+  //for(n=0;n<field.length;n++){
+  //  field[n].onmouseover=function(){
+  //    content[n].style.display='block';
+  //  }
+  //};
 }
 
 
